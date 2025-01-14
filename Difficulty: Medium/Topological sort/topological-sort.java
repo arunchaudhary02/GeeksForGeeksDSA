cca817a8ec6a3e3173199d616081e428808b2f4b
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-01-14 22:54:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            int vertices = Integer.parseInt(read.readLine());
+            int edges = Integer.parseInt(read.readLine());
+
+            for (int i = 0; i < vertices; i++) adj.add(i, new ArrayList<Integer>());
+
+            int p = 0;
+            for (int i = 1; i <= edges; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+            }
+
+            ArrayList<Integer> res = new Solution().topologicalSort(adj);
+
+            if (check(adj, vertices, res) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+            System.out.println("~");
+        }
+    }
+
+    static boolean check(ArrayList<ArrayList<Integer>> adj, int V,
+                         ArrayList<Integer> res) {
+
+        if (V != res.size()) return false;
+
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res.get(i)] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : adj.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return list containing vertices in Topological order.
+    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        // Your code here
+        boolean[] visited = new boolean[adj.size()];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < visited.length; i++) {
+            if(!visited[i]) {
+                dfs(i, visited, stack, adj);
+            }
+        }
+        
+        ArrayList<Integer> topologicalOrder = new ArrayList<>();
+        while(!stack.isEmpty()) {
+            topologicalOrder.add(stack.pop());
+        }
+        
+        return topologicalOrder;
+    }
+    
+    
+    private static void dfs(int node, boolean[] visited, Stack stack, ArrayList<ArrayList<Integer>> adj) {
+        visited[node] = true;
+        for(int neighbor : adj.get(node)) {
+            if(!visited[neighbor]) {
+                dfs(neighbor, visited, stack, adj);
+            }
+        }
+        
+        stack.push(node);
+    }
+}
