3eef634c9242c4727a8b44d68915f95629f395b2
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-01-01 14:21:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,71 +1,24 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
-        for (int g = 0; g < t; g++) {
-            String[] str =
-                (br.readLine()).trim().split(" "); // Reading input as a string array
-            int arr[] = new int[str.length]; // Creating integer array from the input
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-
-            // Getting the result from the Solution class
-            List<Integer> result = new Solution().leaders(arr);
-
-            // Printing the result in the required format
-            if (result.isEmpty()) {
-                System.out.println("[]");
-            } else {
-                for (int i = 0; i < result.size(); i++) {
-                    if (i != 0) System.out.print(" ");
-                    System.out.print(result.get(i));
-                }
-                System.out.println();    // Ensure new line after each test case output
-                System.out.println("~"); // Ensure new line after each test case output
-            }
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
         // code here
+        Stack<Integer> monotonicStack = new Stack<>();
+        
+        int n = arr.length;
+        monotonicStack.push(arr[n - 1]);
+        
+        for(int i = n - 2; i >= 0; i--) {
+            int curr = arr[i];
+            if(curr >= monotonicStack.peek()) {
+                monotonicStack.push(curr);
+            }
+        }
+        
         ArrayList<Integer> result = new ArrayList<>();
         
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = arr.length - 1; i >= 0; i--) {
-            int ele = arr[i];
-            isLeader(ele, stack);
+        while(!monotonicStack.isEmpty()) {
+            result.add(monotonicStack.pop());
         }
         
-        while(!stack.isEmpty()) {
-            result.add(stack.pop());
-        }
         return result;
-        
-    }
-    
-    private static boolean isLeader(int ele, Stack<Integer> stack) {
-        if(stack.isEmpty()) {
-            stack.push(ele);
-            return true;
-        }
-        
-        if(stack.peek() <= ele) {
-            stack.push(ele);
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
