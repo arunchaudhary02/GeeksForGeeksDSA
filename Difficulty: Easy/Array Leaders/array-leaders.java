2a18b5207edb7817e173ef9be9e126386a27c2a2
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-02-11 13:28:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,22 +1,20 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
         // code here
-        Stack<Integer> monotonicStack = new Stack<>();
-        
         int n = arr.length;
-        monotonicStack.push(arr[n - 1]);
+        
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        stack.push(arr[n - 1]);
         
         for(int i = n - 2; i >= 0; i--) {
-            int curr = arr[i];
-            if(curr >= monotonicStack.peek()) {
-                monotonicStack.push(curr);
+            if(stack.peek() <= arr[i]) {
+                stack.push(arr[i]);
             }
         }
         
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        while(!monotonicStack.isEmpty()) {
-            result.add(monotonicStack.pop());
+        while(!stack.isEmpty()) {
+            result.add(stack.pop());
         }
         
         return result;
