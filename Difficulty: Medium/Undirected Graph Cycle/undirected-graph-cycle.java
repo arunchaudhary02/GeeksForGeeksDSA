ad2666ec0df8561c49974905eeb5a878541c98cf
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-10 10:29:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,92 +1,46 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
+// DFS
 class Solution {
-    // Function to detect cycle in an undirected graph.
-    
-    static class Pair {
-        int first, second;
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-    
-    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCycle(int V, int[][] edges) {
         // Code here
-        int vertices = adj.size();
-        boolean[] visited = new boolean[vertices];
-        for(int i = 0; i < vertices; i++) visited[i] = false;
         
-        for(int i = 0; i < vertices; i++) {
-            if(visited[i] == false) {
-                if(checkForCycle(i, vertices, visited, adj)) {
-                    return true;
-                }
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
+        boolean[] visited = new boolean[V];
+        
+        for(int i = 0; i < V; i++) {
+            if(!visited[i] && cycleCheckDFS(i, -1, visited, adj)) {
+                return true;
             }
         }
         
         return false;
     }
     
-    private boolean checkForCycle(int src, int vertices, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+    private boolean cycleCheckDFS(int src, int parent, boolean[] visited, List<List<Integer>> adj) {
+        
         visited[src] = true;
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(src, -1));
         
-        while(!queue.isEmpty()) {
-            int node = queue.peek().first;
-            int parent = queue.peek().second;
-            queue.remove();
-            
-            for(int adjacentNode : adj.get(node)) {
-                if(visited[adjacentNode] == false) {
-                    visited[adjacentNode] = true;
-                    queue.add(new Pair(adjacentNode, node));
-                }
-                else if(parent != adjacentNode) {
+        for(int neighbour : adj.get(src)) {
+            if(visited[neighbour] == false) {
+                if(cycleCheckDFS(neighbour, src, visited, adj)) {
                     return true;
                 }
             }
+            else if(neighbour != parent) {
+                return true;
+            }
         }
+        
         
         return false;
     }
-    
-    
-    
-    
 }
