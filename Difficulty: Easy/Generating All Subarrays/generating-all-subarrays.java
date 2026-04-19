b83dfb7832da7cdd858a45fd41a1b929911c795c
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-19 13:05:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Generating All Subarrays/generating-all-subarrays.java
+++ Difficulty: Easy/Generating All Subarrays/generating-all-subarrays.java
@@ -3,18 +3,18 @@ class Solution {
     public List<List<Integer>> getSubArrays(int[] arr) {
         // code here
         
-        List<List<Integer>> result = new ArrayList<>();
-        
-        for(int i = 0; i < arr.length; i++) {
-            List<Integer> curr = new ArrayList<>();
-            
-            for(int j = i; j < arr.length; j++) {
-                curr.add(arr[j]); 
-                result.add(new ArrayList<>(curr));
-            }
-            
-        }
-        
-        return result;
+       List<List<Integer>> result = new ArrayList<>();
+       
+       for(int i = 0; i < arr.length; i++) {
+           List<Integer> current = new ArrayList<>();
+           for(int j = i; j < arr.length; j++) {
+               current.add(arr[j]);
+               result.add(new ArrayList<>(current));
+           }
+       }
+       
+       return result;
+       
+       
     }
 }
