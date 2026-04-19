fd37ae93a9d82fb5b26d01e75d5150bc9721d0f2
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-19 11:47:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Wave Array/wave-array.java
+++ Difficulty: Medium/Wave Array/wave-array.java
@@ -3,18 +3,14 @@ class Solution {
         // code here
         
         int n = arr.length;
-        if(n == 1) {
+        if(n == 0 || n == 1) {
             return;
         }
         
-        if(n % 2 != 0) {
-            n--;
-        }
-        
-        for(int i = 0; i < n; i+= 2) {
+        for(int i = 1; i < n; i += 2) {
             int temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
         }
         
     }
