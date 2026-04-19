7ac86fc2ccae99247dab122e0f374bcd599b1427
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-19 10:44:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.java
+++ Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.java
@@ -1,3 +1,47 @@
+// Approach 1 
+
+class Solution {
+    public void reverseInGroups(int[] arr, int k) {
+        // code here
+        
+        List<int[]> intervals = new ArrayList<>();
+        int n = arr.length;
+        
+        int startPoint = 0;
+        
+        for(int end = k - 1; end < n; end += k) {
+            intervals.add(new int[] {startPoint, end});
+            startPoint = end + 1;
+        }
+        
+        if(startPoint < n) {
+            intervals.add(new int[] {startPoint, n - 1});
+        }
+        
+        // Finding Answer
+        
+        for(int i = 0; i < intervals.size(); i++) {
+            reverse(arr, intervals.get(i)[0], intervals.get(i)[1]);
+        }
+        
+    }
+    
+    private void reverse(int[] arr, int L, int R) {
+        
+        while(L < R) {
+            int temp = arr[R];
+            arr[R] = arr[L];
+            arr[L] = temp;
+            L++;
+            R--;
+        }
+    }
+}
+
+
+// Approach 2
+
+/*
 class Solution {
     public void reverseInGroups(int[] arr, int k) {
         // code here
@@ -26,3 +70,4 @@ class Solution {
         }
     }
 }
+*/
