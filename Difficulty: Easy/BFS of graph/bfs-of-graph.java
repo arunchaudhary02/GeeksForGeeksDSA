52f4912ca478327ac5b8da6aa39b1e19ebd3eae8
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-07-07 22:10:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,71 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
-    // Function to return Breadth First Search Traversal of given graph.
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
-        ArrayList<Integer> bfs = new ArrayList<>();
         boolean[] visited = new boolean[adj.size()];
-        
+        ArrayList<Integer> bfsResult = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
+        queue.add(0);
         visited[0] = true;
         
         while(!queue.isEmpty()) {
-            int curr = queue.poll();
-            bfs.add(curr);
+            Integer currNode = queue.poll();
+            bfsResult.add(currNode);
             
-            for(Integer it : adj.get(curr)) {
-                if(visited[it] == false) {
-                    visited[it] = true;
-                    queue.offer(it);
+            for(int neighbour : adj.get(currNode)) {
+                if(visited[neighbour] == false) {
+                    visited[neighbour] = true;
+                    queue.offer(neighbour);
                 }
             }
         }
         
-        return bfs;
+        return bfsResult;
     }
 }
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-            for (int i = 0; i < V; i++) {
-                String[] input = br.readLine().trim().split(" ");
-                ArrayList<Integer> node = new ArrayList<>();
-                for (String s : input) {
-                    if (!s.isEmpty()) {
-                        node.add(Integer.parseInt(s));
-                    }
-                }
-                adj.add(node);
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfs(adj);
-            for (int num : ans) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
