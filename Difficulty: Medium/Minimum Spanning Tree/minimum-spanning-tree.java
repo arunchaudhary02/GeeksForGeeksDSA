0b10e78f3b2bc4568f4ff5fde75c5a20cdabd162
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-08-04 17:42:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,80 +1,60 @@
-//{ Driver Code Starts
-
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    static BufferedReader br;
-    static PrintWriter ot;
-
-    public static void main(String args[]) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        ot = new PrintWriter(System.out);
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            int E = Integer.parseInt(br.readLine().trim());
-            List<List<int[]>> list = new ArrayList<>();
-            for (int i = 0; i < V; i++) list.add(new ArrayList<>());
-            for (int i = 0; i < E; i++) {
-                String[] s = br.readLine().trim().split(" ");
-                int a = Integer.parseInt(s[0]);
-                int b = Integer.parseInt(s[1]);
-                int c = Integer.parseInt(s[2]);
-                list.get(a).add(new int[] {b, c});
-                list.get(b).add(new int[] {a, c});
-            }
-            ot.println(new Solution().spanningTree(V, E, list));
-
-            ot.println("~");
-        }
-        ot.close();
-    }
-}
-// } Driver Code Ends
-
-
-// Prim's Algorithm - Minimum Spanning Tree
-class Pair {
-    int node, distance;
-    Pair(int node, int distance) {
-        this.node = node;
-        this.distance = distance;
-    }
-}
-
 class Solution {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        // Code Here.
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> x.distance - y.distance);
-        boolean[] visited = new boolean[V];
+    
+    static class Pair {
+        int weight;
+        int node;
         
+        Pair(int weight, int node) {
+            this.weight = weight;
+            this.node = node;
+        }
+    }
+    public int spanningTree(int V, int[][] edges) {
+        // code here
+        List<List<int[]>> adjList = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2]; 
+            
+            adjList.get(u).add(new int[]{v, w});
+            adjList.get(v).add(new int[]{u, w});
+        }
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> (a.weight - b.weight));
         pq.add(new Pair(0, 0));
+        
         int sum = 0;
+        
+        boolean[] inMst = new boolean[V];
+        
         while(!pq.isEmpty()) {
             Pair curr = pq.poll();
+            int currW = curr.weight;
             int currNode = curr.node;
-            int currWeight = curr.distance;
             
-            if(visited[currNode]) continue;
+            if(inMst[currNode]) {
+                continue;
+            }
+            inMst[currNode] = true;
+            sum += currW;
             
-            visited[currNode] = true;
-            sum += currWeight;
-            
-            for(int[] neighbour : adj.get(currNode)) { //  NextNode = neighbour[0],  weight = neighbour[1]
+            for (int[] neighbour : adjList.get(currNode)) {
                 int nextNode = neighbour[0];
-                int weight = neighbour[1];
+                int nextW = neighbour[1];
                 
-                if (!visited[nextNode]) {
-                    pq.add(new Pair(nextNode, weight));
+                if (!inMst[nextNode]) {
+                    pq.add(new Pair(nextW, nextNode));
                 }
             }
             
         }
         
         return sum;
-        
     }
 }
